2923f3c4785be2797682b613236f5b2237dfc40e
finde <finde.xumara@gmail.com>
2015-04-25 13:36:15 +0200

    another experiments

--- assignment1/moore.sh
+++ assignment1/moore.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 corps=25000
-echo "BASELINE"
+#echo "BASELINE"
+#python ibmmodel/model1_moore.py data/hansards.36.2.e data/hansards.36.2.f data/test.e data/test.f uniform_baseline $corps 0 0 1
+#echo "n-smoothing..."
+#python ibmmodel/model1_moore.py data/hansards.36.2.e data/hansards.36.2.f data/test.e data/test.f uniform_n_0_01 $corps 0.01 0 1
+#python ibmmodel/model1_moore.py data/hansards.36.2.e data/hansards.36.2.f data/test.e data/test.f uniform_n_0_05 $corps 0.05 0 1
+#python ibmmodel/model1_moore.py data/hansards.36.2.e data/hansards.36.2.f data/test.e data/test.f uniform_n_0_10 $corps 0.10 0 1
+#python ibmmodel/model1_moore.py data/hansards.36.2.e data/hansards.36.2.f data/test.e data/test.f uniform_n_0_15 $corps 0.15 0 1
+python ibmmodel/model1_moore.py data/hansards.36.2.e data/hansards.36.2.f data/test.e data/test.f uniform_baseline $corps 0 10000 1
+python ibmmodel/model1_moore.py data/hansards.36.2.e data/hansards.36.2.f data/test.e data/test.f uniform_baseline $corps 0 50000 1
+python ibmmodel/model1_moore.py data/hansards.36.2.e data/hansards.36.2.f data/test.e data/test.f uniform_baseline $corps 0 100000 1
 python ibmmodel/model1_moore.py data/hansards.36.2.e data/hansards.36.2.f data/test.e data/test.f uniform_baseline $corps 0 0 1
-echo "n-smoothing..."
-python ibmmodel/model1_moore.py data/hansards.36.2.e data/hansards.36.2.f data/test.e data/test.f uniform_n_0_01 $corps 0.01 0 1
-python ibmmodel/model1_moore.py data/hansards.36.2.e data/hansards.36.2.f data/test.e data/test.f uniform_n_0_05 $corps 0.05 0 1
-python ibmmodel/model1_moore.py data/hansards.36.2.e data/hansards.36.2.f data/test.e data/test.f uniform_n_0_10 $corps 0.10 0 1
-python ibmmodel/model1_moore.py data/hansards.36.2.e data/hansards.36.2.f data/test.e data/test.f uniform_n_0_15 $corps 0.15 0 1
+python ibmmodel/model1_moore.py data/hansards.36.2.e data/hansards.36.2.f data/test.e data/test.f uniform_baseline $corps 0 0 2
+python ibmmodel/model1_moore.py data/hansards.36.2.e data/hansards.36.2.f data/test.e data/test.f uniform_baseline $corps 0 0 3
+# random selection
+python ibmmodel/model1_moore.py data/hansards.36.2.e data/hansards.36.2.f data/test.e data/test.f uniform_n_0_01 $corps 0.01 100000 3
